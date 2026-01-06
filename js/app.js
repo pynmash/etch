3a66e53cb61126239cd7b33b5aefd6d30008469c
Dan Toms <157254317+pynmash@users.noreply.github.com>
2026-01-06 19:10:22 +0000

    Set square size based on number of squares

--- js/app.js
+++ js/app.js
@@ -1,16 +1,23 @@
 const container = document.querySelector('#container');
-const pointsOnAxis = 16;
+const pointsOnAxis = 100;
 
-for (let i = 0; i < pointsOnAxis; i++) {
-    const row = document.createElement('div')
-    row.classList.add('row');
-    for (let j = 0; j < pointsOnAxis; j++) {
-        const square = document.createElement('div')
-        square.classList.add('square');
-        row.appendChild(square);
+function createContainer(points) {
+    const size = 800 / points;
+    for (let i = 0; i < points; i++) {
+        const row = document.createElement('div')
+        row.classList.add('row');
+        for (let j = 0; j < points; j++) {
+            const square = document.createElement('div')
+            square.classList.add('square');
+            square.style.height = size.toString() + 'px';
+            square.style.width = size.toString() + 'px';
+            row.appendChild(square);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
 }
+
+createContainer(pointsOnAxis);
 
 container.addEventListener('mouseover', (e) => {
     if (e.target.classList.contains('square')) {
