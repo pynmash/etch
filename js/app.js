e595ba459307c770e85e5e26e392d764a013fa1f
Dan Toms <157254317+pynmash@users.noreply.github.com>
2026-01-06 16:42:19 +0000

    Make squares turn red when the cursor passes over them

--- js/app.js
+++ js/app.js
@@ -1,12 +1,20 @@
 const container = document.querySelector('#container');
+const pixelColor = 'red'
+const pointsOnAxis = 16;
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < pointsOnAxis; i++) {
     const row = document.createElement('div')
     row.classList.add('row');
-    for (let j = 0; j < 16; j++) {
+    for (let j = 0; j < pointsOnAxis; j++) {
         const square = document.createElement('div')
         square.classList.add('square');
         row.appendChild(square);
     }
     container.appendChild(row);
 }
+
+container.addEventListener('mouseover', (e) => {
+    if (e.target.classList.contains('square')) {
+        e.target.style.backgroundColor = pixelColor;
+    }
+})
