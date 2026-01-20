1e3a5936d2d719ee23e4cb29882563fdcca108f9
Dan Toms <157254317+pynmash@users.noreply.github.com>
2026-01-20 11:42:59 +0000

    Add random color

--- js/app.js
+++ js/app.js
@@ -1,75 +1,90 @@
 const screenWidth = document.documentElement.clientWidth / 3;
-const container = document.querySelector('#container');
-const sizeInput = document.querySelector('#size-input');
-const setSizeBtn = document.querySelector('#size');
-const gridlinesToggle = document.querySelector('#gridlines');
-const colorButtons = document.querySelector('.colors');
+const container = document.querySelector("#container");
+const sizeInput = document.querySelector("#size-input");
+const setSizeBtn = document.querySelector("#size");
+const gridlinesToggle = document.querySelector("#gridlines");
+const colorButtons = document.querySelector(".colors");
 const pointsOnAxis = 16;
 
-let penColor = 'blue';
+let penColor = "blue";
 let penUp = false;
 
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
 function createContainer(points) {
-    gridlinesToggle.checked = false;
-    const size = screenWidth / points;
-    for (let i = 0; i < points; i++) {
-        const row = document.createElement('div')
-        row.classList.add('row');
-        for (let j = 0; j < points; j++) {
-            const square = document.createElement('div')
-            square.classList.add('square');
-            square.style.height = size.toString() + 'px';
-            square.style.width = size.toString() + 'px';
-            row.appendChild(square);
-        }
-        container.appendChild(row);
+  gridlinesToggle.checked = false;
+  const size = screenWidth / points;
+  for (let i = 0; i < points; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    for (let j = 0; j < points; j++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      square.style.height = size.toString() + "px";
+      square.style.width = size.toString() + "px";
+      row.appendChild(square);
     }
+    container.appendChild(row);
+  }
 }
 
 function destroyContainer() {
-    const rows = container.querySelectorAll('.row');
-    for (row of rows) {
-        row.remove();
-    }
+  const rows = container.querySelectorAll(".row");
+  for (row of rows) {
+    row.remove();
+  }
 }
 
 createContainer(pointsOnAxis);
 
-container.addEventListener('mouseover', (e) => {
-    if (e.target.classList.contains('square') && penUp === false) {
-        e.target.classList.add(penColor);
+container.addEventListener("mouseover", (e) => {
+  if (e.target.classList.contains("square") && penUp === false) {
+    if (penColor.startsWith("rgb")) {
+      e.target.style.backgroundColor = penColor;
+    } else {
+      e.target.classList.add(penColor);
     }
-})
+  }
+});
 
-setSizeBtn.addEventListener('click', () => {
-    destroyContainer();
-    createContainer(sizeInput.value)
-})
+setSizeBtn.addEventListener("click", () => {
+  destroyContainer();
+  createContainer(sizeInput.value);
+});
 
-colorButtons.addEventListener('click', (e) => {
-    penColor = e.target.id
-})
+colorButtons.addEventListener("click", (e) => {
+  if (e.target.id == "random") {
+    penColor = getRandomColor();
+  } else {
+    penColor = e.target.id;
+  }
+});
 
+gridlinesToggle.addEventListener("change", () => {
+  const squares = document.querySelectorAll(".square");
+  for (square of squares) {
+    square.classList.toggle("border");
+  }
+});
 
-gridlinesToggle.addEventListener('change', () => {
-    const squares = document.querySelectorAll('.square');
-    for (square of squares) {
-        square.classList.toggle('border');
-    }
-})
+document.addEventListener("keydown", (e) => {
+  const keyName = e.key;
 
-document.addEventListener('keydown', (e) => {
-    const keyName = e.key;
+  if (keyName === "Control") {
+    penUp = true;
+  }
+});
 
-    if (keyName === 'Control') {
-        penUp = true;
-    }
-})
+document.addEventListener("keyup", (e) => {
+  const keyName = e.key;
 
-document.addEventListener('keyup', (e) => {
-    const keyName = e.key;
-
-    if (keyName === 'Control') {
-        penUp = false;
-    }
-})
+  if (keyName === "Control") {
+    penUp = false;
+  }
+});
